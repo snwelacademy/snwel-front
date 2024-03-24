1f22aa5dfbacbada100c28a4c6ae04748699a5f7
shivam-687 <shivamsi687@gmail.com>
2024-03-24 11:57:09 +0530

    add mobile menu

--- src/components/navbar/MobileNavbar.tsx
+++ src/components/navbar/MobileNavbar.tsx
@@ -2,7 +2,6 @@ import {
     Drawer,
     DrawerClose,
     DrawerContent,
-    DrawerDescription,
     DrawerFooter,
     DrawerHeader,
     DrawerTitle,
@@ -12,21 +11,40 @@ import {
 import { Button } from '../ui/button'
 import { MenuIcon } from 'lucide-react'
 import Logo from "../shared/Logo"
+import menus from "@/data/menu"
+import { nanoid } from "nanoid"
+import { Link } from "react-router-dom"
+import { constants } from "@/config/constants"
+
 
 const MobileNavbar = () => {
     return (
         <Drawer direction="left" shouldScaleBackground>
             <DrawerTrigger asChild ><Button><MenuIcon/></Button></DrawerTrigger>
-            <DrawerContent className="h-full rounded-none rounded-tr-2xl rounded-br-2xl w-[80%]">
-                <DrawerHeader>
+            <DrawerContent className="h-full rounded-none rounded-tr-2xl rounded-br-2xl w-[80%] overflow-hidden" style={{backgroundImage: `url(${constants.imagePath+'/mobile-menu-bg.jpg'})`}}>
+                <div className="absolute top-0 left-0 right-0 bottom-0 w-full h-full -z-10 bg-white"></div>
+                <DrawerHeader className="justify-center">
                     <DrawerTitle><Logo/></DrawerTitle>
-                    <DrawerDescription>This action cannot be undone.</DrawerDescription>
                 </DrawerHeader>
+                <div className="h-full flex  flex-col items-center justify-center">
+                    {
+                        menus.map(menu => {
+                            return <div className="px-4 py-2 text-xl font-extralight text-primary  ">
+                             <Link to={menu.link||'#'} className="" key={nanoid()} >
+                                <span className="font-bold text-destructive text-2xl">{menu.label.charAt(0)}</span>
+                                {menu.label.slice(1)}
+                            </Link>
+                            </div>
+                        })
+                    }
+                </div>
                 <DrawerFooter>
-                    <Button>Submit</Button>
                     <DrawerClose>
-                        <Button variant="outline">Cancel</Button>
+                        <a href="mail:shivamsi687@gmail.com"><Button className="w-full">Email Us</Button></a>
                     </DrawerClose>
+                    {/* <DrawerClose>
+                        <Button variant="outline">Cancel</Button>
+                    </DrawerClose> */}
                 </DrawerFooter>
             </DrawerContent>
         </Drawer>
